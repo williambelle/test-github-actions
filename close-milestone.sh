5ae04070a13b7a205cd11a6697a4c51589cb9faa
William Belle <william.belle@gmail.com>
2020-04-02 20:27:52 +0200

    test

--- close-milestone.sh
+++ close-milestone.sh
@@ -2,18 +2,22 @@
 
 set -eo pipefail
 
-R_GITHUB_API="https://api.github.com/repos"
-MILESTONES_URL="$R_GITHUB_API/${GITHUB_REPOSITORY}/milestones?per_page=100"
+API_HOST="https://api.github.com/repos"
+MILESTONES_URL="$API_HOST/${GITHUB_REPOSITORY}/milestones?per_page=100"
 
+echo -e ${GITHUB_REPOSITORY} 
 if [ $(echo ${GITHUB_REPOSITORY} | wc -c) -eq 1 ] ; then
   echo -e "\033[31mRepository cannot be empty\033[0m"
   exit 1
 fi
 
 echo -e ${GITHUB_REF#refs/heads/}
+echo -e $MILESTONES_URL
+
 curl --silent --request GET \
   --url $MILESTONES_URL \
   --header "Authorization: Bearer ${GITHUB_TOKEN}"
+
 NUMBER=`curl --silent --request GET \
   --url $MILESTONES_URL \
   --header "Authorization: Bearer ${GITHUB_TOKEN}" | jq -r ".[]|select(.title==\"v${GITHUB_REF#refs/heads/}\").number"`
@@ -26,7 +30,7 @@ if [ $(echo $NUMBER | wc -c) -eq 1 ] ; then
 fi
 
 curl --silent --request PATCH \
-  --url $R_GITHUB_API/${GITHUB_REPOSITORY}/milestones/$NUMBER \
+  --url $API_HOST/${GITHUB_REPOSITORY}/milestones/$NUMBER \
   --header "Authorization: Bearer ${GITHUB_TOKEN}" \
   --header 'Content-Type: application/json' \
   --data '{"state":"closed"}'
