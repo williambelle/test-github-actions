d0f3be3279ed9319b0817339f18962fc9610c63a
William Belle <william.belle@gmail.com>
2020-04-02 18:09:33 +0200

    test

--- close-milestone.sh
+++ close-milestone.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eo pipefail
 
-$GITHUB_API_R="https://api.github.com/repos"
-$MILESTONES_URL="$GITHUB_API_R/${GITHUB_REPOSITORY}/milestones?per_page=100"
+GITHUB_API_R="https://api.github.com/repos"
+MILESTONES_URL="$GITHUB_API_R/${GITHUB_REPOSITORY}/milestones?per_page=100"
 
 if [ $(echo ${GITHUB_REPOSITORY} | wc -c) -eq 1 ] ; then
   echo -e "\033[31mRepository cannot be empty\033[0m"
   exit 1
 fi
 
-$NUMBER=`curl --url $MILESTONES_URL | jq -r ".[]|select(.title==\"v${GITHUB_REF}\").number"`
+NUMBER=`curl --url $MILESTONES_URL | jq -r ".[]|select(.title==\"v${GITHUB_REF}\").number"`
 
 if [ $(echo $NUMBER | wc -c) -eq 1 ] ; then
   echo -e "\033[31mMilestone number cannot be empty\033[0m"
